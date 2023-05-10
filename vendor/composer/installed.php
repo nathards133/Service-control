39bbe34c5dbd4d3297bfe0fdbbc6194a8f160eb7
nathafaculdade <nathafaculdade@gmail.com>
2023-05-10 09:56:04 -0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'seu_nome/gestao-servico',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '177640512c4d7d2e37a80902eb05ad4aa9d1f852',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'seu_nome/gestao-servico' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '177640512c4d7d2e37a80902eb05ad4aa9d1f852',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
